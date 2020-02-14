eb1b8dadd41e4e525e046b504537e835a098e69f
Adam Petroff <adam.petroff@thefuntasty.com>
2020-02-14 23:22:18 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -15,50 +15,70 @@ function App() {
 
   const videoRef = useRef<HTMLVideoElement>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const canvas2Ref = useRef<HTMLCanvasElement>(null)
 
-  const run = async (bodyPix, ctx: CanvasRenderingContext2D) => {
-    const result = await bodyPix.segmentWithParts()
-    const arr = Array.from(result.segmentation.data)
+  const run = async (uNet, ctx: CanvasRenderingContext2D, video) => {
+    const result = await uNet.segment(video)
+    // console.log(result)
+    const arr = Array.from(result.raw.backgroundMask)
 
-    const imgData = ctx.createImageData(result.segmentation.width, result.segmentation.height)
+    const imgData = ctx.createImageData(384, 384)
     const { data } = imgData
 
+    console.log(data, arr, result)
     for(let i = 0; i < arr.length; i += 1) {
-      const j = i * 4
-      if(arr[i] === 0 || arr[i] === 1){
-        data[j] = 0
-        data[j+1] = 0
-        data[j+2] = 0
-        data[j+3] = 255
-      }
+      // if(arr[i]){
+      //   data[i] = arr[i]
+      // }
+
+      const j = i * 3
+      // if(arr[i] === 0 || arr[i] === 1){
+        data[j] = arr[i]
+        data[j+1] = arr[i]
+        data[j+2] = arr[i]
+        // data[j+3] = 255
+      // }
     }
     ctx.putImageData(imgData, 0, 0)
 
-    run(bodyPix, ctx)
+    run(uNet, ctx, video)
   }
 
   const setup = async (video) => {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true })
     video.srcObject = stream;
     video.onloadedmetadata = function() {
-      setWidth(video.videoWidth / 4)
-      setHeight(video.videoHeight / 4)
-   };
-    
-    return await ml5.bodyPix(video, options)
+      // setWidth(video.videoWidth / 2)
+      // setHeight(video.videoHeight / 2)
+    };
+
+    const uNet = ml5.uNet('face');
+
+    return uNet
+  }
+
+  const loadBackground = (ctx) => {
+    const image = new Image();
+    image.src = 'paradise.jpg';
+    image.onload = function(){
+      ctx.drawImage(image, 0, 0);
+    }
   }
 
   useEffect(() => { 
-    if(!videoRef.current || !canvasRef.current){ 
+    if(!videoRef.current || !canvasRef.current || !canvas2Ref.current){ 
       return
     }
+
+    const resultContext = canvas2Ref.current.getContext("2d")
+    // loadBackground(resultContext)
 
     const ctx = canvasRef.current.getContext("2d")
     if(!ctx) {
       throw new Error("no context :(")
     }
 
-    setup(videoRef.current).then(bodyPix => run(bodyPix, ctx))
+    setup(videoRef.current).then(uNet => run(uNet, resultContext, videoRef.current))
   }, []);
 
   return (
@@ -66,6 +86,10 @@ function App() {
       <div className="video-wrapper">
         <video className="video" width={width} height={height} autoPlay={true} ref={videoRef} />
         <canvas width={width} height={height} className="canvas" ref={canvasRef} />
+      </div>
+      <div className="video-wrapper">
+        <img src="paradise.jpg" width={width} height={width} style={{ position: "absolute" }} />
+        <canvas width={width} height={width} className="canvas" ref={canvas2Ref} />
       </div>
     </div>
   );
